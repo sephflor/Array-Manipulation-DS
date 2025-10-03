dac590def5707eef20a88686fb73c98b29833e93
Sephanne Flor <sephanneflor9@gmail.com>
2025-10-03 00:55:44 -0700

    Create Array Manipulation.cs

---
+++ Array Manipulation.cs
@@ -0,0 +1,82 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'arrayManipulation' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static long arrayManipulation(int n, List<List<int>> queries)
+    {
+         long[] arr = new long[n + 2]; 
+        
+        foreach (var q in queries)
+        {
+            int a = q[0];
+            int b = q[1];
+            int k = q[2];
+
+            arr[a] += k;
+            if (b + 1 <= n)
+                arr[b + 1] -= k;
+        }
+
+        
+        long max = 0;
+        long current = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            current += arr[i];
+            if (current > max)
+                max = current;
+        }
+
+        return max;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<List<int>> queries = new List<List<int>>();
+
+        for (int i = 0; i < m; i++)
+        {
+            queries.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(queriesTemp => Convert.ToInt32(queriesTemp)).ToList());
+        }
+
+        long result = Result.arrayManipulation(n, queries);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
